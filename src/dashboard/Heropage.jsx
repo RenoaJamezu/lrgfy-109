a72dcc22688d6e79ae6192c07374934d41f9ac53
Kurasao <jamerolenor@gmail.com>
2023-11-21 10:36:28 +0800

    LOGO update

--- src/dashboard/Heropage.jsx
+++ src/dashboard/Heropage.jsx
@@ -5,7 +5,10 @@ const Heropage = () => {
         <div className="flex justify-center items-center">
           <div className="flex items-center">
             <div>
-              <img src="/public/images/LRGfy-LOGO.png" className="w-[663px]"/>
+              <img
+              src="../public/images/LRGfy-LOGO.png"
+              alt="LOGO"
+              className="w-[663px]"/>
             </div>
           </div>
         </div>
